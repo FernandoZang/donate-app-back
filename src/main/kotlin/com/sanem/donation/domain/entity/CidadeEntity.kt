fa703de11de755442a95eeac3141803f2a44156f
fzang <fzang@uolinc.com>
2025-05-06 12:01:23 -0300

    feat:[DA-37] - Add Cidade Entity

---
+++ src/main/kotlin/com/sanem/donation/domain/entity/CidadeEntity.kt
@@ -0,0 +1,16 @@
+package com.sanem.donation.domain.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "CIDADE")
+class CidadeEntity(
+    @Id
+    @Column(name = "ID", nullable = false)
+    var id: Int,
+    @Column(name = "NOME", nullable = false)
+    var nome: String
+)
