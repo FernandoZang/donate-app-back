3d3f954c2881d40cb849bc16325efa6366506d7d
fzang <fzang@uolinc.com>
2025-05-06 10:02:46 -0300

    [DA-37] - Create project

---
+++ src/test/kotlin/com/sanem/donation/DonationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sanem.donation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DonationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
