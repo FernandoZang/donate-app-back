77df652560859589f3535221c0dce3ddea0a60b7
fzang <fzang@uolinc.com>
2025-06-17 00:42:34 -0300

    fix:[DA-37] - Apply Ktlint

--- src/main/kotlin/com/sanem/donation/services/UsuarioService.kt
+++ src/main/kotlin/com/sanem/donation/services/UsuarioService.kt
@@ -6,7 +6,6 @@ import com.sanem.donation.domain.repository.UsuarioRepository
 import com.sanem.donation.exceptions.ForbiddenException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
-import org.springframework.web.client.HttpClientErrorException
 
 @Service
 class UsuarioService(
@@ -19,13 +18,12 @@ class UsuarioService(
         if (
             rules.contains(RegraAcesso(nome = "MASTER")) ||
             rules.contains(RegraAcesso(nome = "SYS")) ||
-            rules.contains(RegraAcesso(nome = "ADMIN")))
-        {
+            rules.contains(RegraAcesso(nome = "ADMIN"))
+        ) {
             throw ForbiddenException(
                 message = "Sem premissão para cadastrar esse tipo de usuário"
             )
-        }
-        else {
+        } else {
             return usuarioRepository.saveAndFlush(usuarioEntity)
         }
     }
