fa0362274cf795140f354027b0187e99b49a2005
fzang <fzang@uolinc.com>
2025-06-22 21:47:19 -0300

    fix:[DA-37] - Apply KTLINT

--- src/main/kotlin/com/sanem/donation/config/SecurityConfig.kt
+++ src/main/kotlin/com/sanem/donation/config/SecurityConfig.kt
@@ -1,6 +1,5 @@
 package com.sanem.donation.config
 
-import org.apache.catalina.filters.CorsFilter
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -55,7 +54,6 @@ class SecurityConfig {
             .role("GERENTE").implies("ATENDENTE")
             .build()
     }
-
 
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
